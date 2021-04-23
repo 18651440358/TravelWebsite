121934bbfdd7e2beb894cd550f2a60bb6313d72e
周灿 <2657487207@qq.com>
2021-04-24 00:53:25 +0800

    4.23

---
+++ src/pages/landing/Landing.vue
@@ -0,0 +1,16 @@
+<template>
+<!-- 系统着落页 -->
+  <div class="zc-page-landing">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "zc-page-landing"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
