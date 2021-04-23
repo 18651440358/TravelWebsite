a61b19fff8ceed6d254fd31471686079f86268d5
周灿 <2657487207@qq.com>
2021-04-24 01:45:36 +0800

    4.25

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,32 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  mounted() {
+    // 检测主题
+    this.checkTheme();
+  },
+  methods: {
+    // 检测主题
+    checkTheme() {
+      var theme = localStorage.getItem("theme");
+      window.document.documentElement.setAttribute(
+          "theme",
+          theme ? theme : "light"
+      );
+    }
+  }
+}
+</script>
+
 <style lang="scss">
+  @import "./assets/style/theme/_handle.scss";
   // 去除浏览器默认样式
   body{
     width: 100vw;
+    @include fontColor(primaryText);
   }
   a{
     text-decoration: none;
