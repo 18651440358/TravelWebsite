b8dd427f5eb182d1efad280c576876cb2a40f8c5
周灿 <2657487207@qq.com>
2021-04-26 09:38:16 +0800

    4.25

---
+++ src/component/col/README.md
@@ -0,0 +1,18 @@
+# Col
+列容器
+
+## How to use
+
+```vue
+<zc-row>
+  Page content ...
+</zc-row>
+```
+
+## Attributes
+
+| Attribute | Description | Type | Accepted Values | Default |
+|---|---|---|---|---|
+| grid | Number of columns occupied by the grid | Number | Cannot exceed "24" | 24 |
+| offset | Number of divisions on the left side of the grid | Number | - | 0 |
+| tag | Custom element tag | String | - | div |
--- src/pages/landing/Landing.vue
+++ src/pages/landing/Landing.vue
@@ -2,17 +2,16 @@
 <!-- 系统着落页 -->
   <div class="zc-page-landing">
     <zc-container>
-      <zc-row flex justify="center">11</zc-row>
+
     </zc-container>
   </div>
 </template>
 
 <script>
 import ZcContainer from "@/component/container/container";
-import ZcRow from "@/component/row/row";
 export default {
   name: "zc-page-landing",
-  components: {ZcRow, ZcContainer}
+  components: {ZcContainer}
 }
 </script>
 
