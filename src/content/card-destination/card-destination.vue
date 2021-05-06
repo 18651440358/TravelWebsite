2ddad950735c1600fc7e058ce2665ac99c35f5e7
周灿 <2657487207@qq.com>
2021-05-06 20:32:15 +0800

    Revert "4.25"
    This reverts commit 7fc0c4c5

--- src/content/card-destination/card-destination.vue
+++ src/content/card-destination/card-destination.vue
@@ -1,11 +1,11 @@
 <template>
-<!--  目的地-->
+  <!--  目的地-->
   <div class="card-destination">
-<!--    封面-->
+    <!--    封面-->
     <div class="destination-cover">
       <img :src="coverSrc"  :alt="title"/>
     </div>
-<!--    文字-->
+    <!--    文字-->
     <div class="destination-content swiper-no-swiping">
       <h3 v-if="$i18n.locale === 'zh-CN'">{{title}}</h3>
       <h3 v-else>{{titleEn}}</h3>
@@ -65,45 +65,45 @@ export default {
 
 <style scoped lang="scss">
 @import "../../assets/style/theme/_handle.scss";
-  .destination-cover{
-    width: 100%;
-    padding-top: 75%;
-    position: relative;
-    border-radius: 1rem;
-    overflow: hidden;
-    @include box-shadow(cardBoxShadow);
-  }
-  .destination-cover > img{
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    object-position: center;
-    object-fit: cover;
-    top: 0;
-  }
-  // 内容
-  .destination-content{
-    margin-top: 10px;
-  }
-  .destination-content h3{
-    font-size: 14px;
-    font-weight: 700;
-  }
-  .hotel-content{
-    display: flex;
-    align-items: center;
-    @include fontColor(regularText);
-    font-size: 12px;
-    font-weight: 500;
-  }
-  .hotel-content .number{
-    margin-right: 5px;
-  }
-  .hotel-content .avg{
-    font-weight: 600;
-    margin-left: 3px;
-    @include fontColor(grayText);
-  }
+.destination-cover{
+  width: 100%;
+  padding-top: 75%;
+  position: relative;
+  border-radius: 1rem;
+  overflow: hidden;
+  @include box-shadow(cardBoxShadow);
+}
+.destination-cover > img{
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  object-position: center;
+  object-fit: cover;
+  top: 0;
+}
+// 内容
+.destination-content{
+  margin-top: 10px;
+}
+.destination-content h3{
+  font-size: 14px;
+  font-weight: 700;
+}
+.hotel-content{
+  display: flex;
+  align-items: center;
+  @include fontColor(regularText);
+  font-size: 12px;
+  font-weight: 500;
+}
+.hotel-content .number{
+  margin-right: 5px;
+}
+.hotel-content .avg{
+  font-weight: 600;
+  margin-left: 3px;
+  @include fontColor(grayText);
+}
 </style>
 
 <i18n>
